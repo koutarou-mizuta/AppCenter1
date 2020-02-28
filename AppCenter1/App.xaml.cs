9581c50f62a8ebe3efd9022596a61bd32e6d3fc4
mousePC <koutarou.mizuta@findtech.jp>
2020-02-28 20:15:08 +0900

    プロジェクト ファイルを追加します。

---
+++ AppCenter1/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AppCenter1.Services;
+using AppCenter1.Views;
+
+namespace AppCenter1
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
